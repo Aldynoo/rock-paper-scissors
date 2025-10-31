79ab1fbb7022c94e8454d5c8bb951e6ebe13e187
Aldynoo <aldinidrizoski9@gmail.com>
2025-10-31 21:28:50 +0100

    made logic that pushes numbers from 0 to 99... and 33 is threshold for each option

--- javascript.js
+++ javascript.js
@@ -1,2 +1,15 @@
-console.log("hello world")
-console.log("bruv")
+
+function getComputerChoice() {
+    let choice = (Math.random()* 100);
+
+    if (choice < 33) {
+        return "rock";
+    }
+    else if (choice < 6) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+console.log(getComputerChoice());
