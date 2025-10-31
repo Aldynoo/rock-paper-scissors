66bfdba5c96e2ad033d712ee5fc04d4ec191747e
Aldynoo <aldinidrizoski9@gmail.com>
2025-11-01 00:43:58 +0100

    made logic for comp and human and playing round

--- javascript.js
+++ javascript.js
@@ -1,15 +1,48 @@
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
-    let choice = (Math.random()* 100);
+    let computerChoice = (Math.random()* 100);
 
-    if (choice < 33) {
+    if (computerChoice < 33) {
         return "rock";
-    }
-    else if (choice < 6) {
+    } else if (computerChoice < 66) {
         return "paper";
-    }
-    else {
+    } else {
         return "scissors";
     }
 }
 console.log(getComputerChoice());
+
+
+function getHumanChoice () {
+    let humanChoice = parseInt(prompt("Enter 1 for 'rock', 2 for 'paper' or 3 for 'scissors'"));
+
+    if (humanChoice === 1) {
+        return "rock";
+    } else if (humanChoice === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+console.log(getHumanChoice());
+
+
+function playRound (humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return "It's a tie";
+    }
+    else if (
+            (humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper")
+            ){
+                return "You win!"
+            }
+    else 
+        {
+            return "You lose!"
+        }
+    }
+console.log(playRound());
